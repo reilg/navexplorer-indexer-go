9ff802f0ac919a88dc6c4ffbc2ecdfc69efdbe20
Dan Tudor <dtudor01@gmaiol.com>
2019-10-18 12:31:47 +0100

    Unexpected ,

--- pkg/explorer/address_transaction.go
+++ pkg/explorer/address_transaction.go
@@ -6,7 +6,7 @@ type AddressTransaction struct {
 	Height uint64       `json:"height"`
 	Time   int64        `json:"time, omitempty"`
 	Type   TransferType `json:"type"`
-	Input  uint64       `json:"input,"`
+	Input  uint64       `json:"input"`
 	Output uint64       `json:"output"`
 	Total  int64        `json:"total"`
 }
