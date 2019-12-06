0d0cbb4143d2d30168d49b5264324c055c206b44
Dan Tudor <dtudor01@gmaiol.com>
2019-12-06 15:01:38 +0000

    Adds GetByHash

--- internal/service/dao/proposal/proposals.go
+++ internal/service/dao/proposal/proposals.go
@@ -1,17 +1,29 @@
 package proposal
 
-import "github.com/NavExplorer/navexplorer-indexer-go/pkg/explorer"
+import (
+	"github.com/NavExplorer/navexplorer-indexer-go/pkg/explorer"
+)
 
 var Proposals proposals
 
 type proposals []*explorer.Proposal
 
+func (p *proposals) GetByHash(hash string) *explorer.Proposal {
+	for _, p := range Proposals {
+		if p.Hash == hash {
+			return p
+		}
+	}
+
+	return nil
+}
+
 func (p *proposals) Delete(hash string) {
 	for i, _ := range Proposals {
 		if Proposals[i].Hash == hash {
-			Proposals[i] = Proposals[len(Proposals)-1] // Copy last element to index i.
-			Proposals[len(Proposals)-1] = nil          // Erase last element (write zero value).
-			Proposals = Proposals[:len(Proposals)-1]   // Truncate slice.
+			Proposals[i] = Proposals[len(Proposals)-1]                                     // Copy last element to index i.
+			Proposals[len(Proposals)-1] = nil                                              // Erase last element (write zero value).
+			Proposals = append([]*explorer.Proposal(nil), Proposals[:len(Proposals)-1]...) // Truncate slice.
 			break
 		}
 	}
