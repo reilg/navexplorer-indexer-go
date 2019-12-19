6b9eac5f2f8adc7b4d6bd9a5ac353795b20ed97f
Dan Tudor <dtudor01@gmaiol.com>
2019-12-19 11:48:32 +0000

    Comment code after fatal

--- internal/service/address/indexer.go
+++ internal/service/address/indexer.go
@@ -58,9 +58,9 @@ func (i *Indexer) Index(txs []*explorer.BlockTransaction) {
 		)
 	}
 
-	if addresses, err := i.repo.GetAddresses(hashes); err != nil {
+	if _, err := i.repo.GetAddresses(hashes); err != nil {
 		log.WithError(err).Fatalf("Could not get addresses for txs at height %d", txs[0].Height)
-		addresses = i.createNewAddresses(hashes, addresses)
+		//addresses = i.createNewAddresses(hashes, addresses)
 	}
 }
 
@@ -76,10 +76,7 @@ func (i *Indexer) createNewAddresses(hashes []string, addresses []*explorer.Addr
 		}()
 
 		if !found {
-			addresses = append(addresses, &explorer.Address{
-				MetaData: explorer.MetaData{Dirty: true},
-				Hash:     hash,
-			})
+			addresses = append(addresses, &explorer.Address{Hash: hash})
 		}
 	}
 
