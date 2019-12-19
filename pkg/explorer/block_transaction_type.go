46ec4f9e6b9e5e27a0aaad1a3bd0583e2a136222
Dan Tudor <dtudor01@gmaiol.com>
2019-12-19 11:04:55 +0000

    Remove unused blockTxType

--- pkg/explorer/block_transaction_type.go
+++ pkg/explorer/block_transaction_type.go
@@ -3,10 +3,9 @@ package explorer
 type BlockTransactionType string
 
 var (
-	TxCoinbase     BlockTransactionType = "coinbase"
-	TxStaking      BlockTransactionType = "staking"
-	TxColdStaking  BlockTransactionType = "cold_staking"
-	TxPoolStaking  BlockTransactionType = "pool_staking"
-	TxStakeDeposit BlockTransactionType = "staking_deposit"
-	TxSpend        BlockTransactionType = "spend"
+	TxCoinbase    BlockTransactionType = "coinbase"
+	TxStaking     BlockTransactionType = "staking"
+	TxColdStaking BlockTransactionType = "cold_staking"
+	TxPoolStaking BlockTransactionType = "pool_staking"
+	TxSpend       BlockTransactionType = "spend"
 )
