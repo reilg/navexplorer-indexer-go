86c43dbf280466d46e0868dcbd10a7be51182dc5
Dan Tudor <dtudor01@gmaiol.com>
2020-01-18 20:13:47 +0000

    Adds address balance to address tx

--- pkg/explorer/address_transaction.go
+++ pkg/explorer/address_transaction.go
@@ -5,13 +5,14 @@ import "time"
 type AddressTransaction struct {
 	MetaData MetaData `json:"-"`
 
-	Hash   string       `json:"hash"`
-	Txid   string       `json:"txid"`
-	Height uint64       `json:"height"`
-	Time   time.Time    `json:"time, omitempty"`
-	Type   TransferType `json:"type"`
-	Input  uint64       `json:"input"`
-	Output uint64       `json:"output"`
-	Total  int64        `json:"total"`
-	Cold   bool         `json:"cold"`
+	Hash    string       `json:"hash"`
+	Txid    string       `json:"txid"`
+	Height  uint64       `json:"height"`
+	Time    time.Time    `json:"time, omitempty"`
+	Type    TransferType `json:"type"`
+	Input   uint64       `json:"input"`
+	Output  uint64       `json:"output"`
+	Total   uint64       `json:"total"`
+	Balance uint64       `json:"balance"`
+	Cold    bool         `json:"cold"`
 }
