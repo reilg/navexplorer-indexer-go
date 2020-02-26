f14fe3cfddfb34d27163c9c73f8bdf98f1f2333b
Dan Tudor <dtudor01@gmaiol.com>
2020-02-26 23:34:28 +0000

    Adds paid status

--- pkg/explorer/cfund_payment_request_status.go
+++ pkg/explorer/cfund_payment_request_status.go
@@ -5,6 +5,7 @@ type PaymentRequestStatus string
 var (
 	PaymentRequestPending  PaymentRequestStatus = "pending"
 	PaymentRequestAccepted PaymentRequestStatus = "accepted"
+	PaymentRequestPaid     PaymentRequestStatus = "paid"
 	PaymentRequestRejected PaymentRequestStatus = "rejected"
 	PaymentRequestExpired  PaymentRequestStatus = "expired"
 )
@@ -14,6 +15,8 @@ func PaymentRequestStatusIsValid(status string) bool {
 	case status == string(PaymentRequestPending):
 		return true
 	case status == string(PaymentRequestAccepted):
+		return true
+	case status == string(PaymentRequestPaid):
 		return true
 	case status == string(PaymentRequestRejected):
 		return true
