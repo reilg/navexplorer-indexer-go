fe325c4068a3b7c623d5aec1d08b5ec40618557d
Dan Tudor <dtudor01@gmaiol.com>
2020-02-28 16:28:10 +0000

    Adds cfund abstain vout type support

--- pkg/explorer/vout_type.go
+++ pkg/explorer/vout_type.go
@@ -3,31 +3,35 @@ package explorer
 type VoutType string
 
 var (
-	VoutNonstandard           VoutType = "nonstandard"
-	VoutNulldata              VoutType = "nulldata"
-	VoutPubkeyhash            VoutType = "pubkeyhash"
-	VoutPubkey                VoutType = "pubkey"
-	VoutScripthash            VoutType = "scripthash"
-	VoutColdStaking           VoutType = "cold_staking"
-	VoutCfundContribution     VoutType = "cfund_contribution"
-	VoutProposalNoVote        VoutType = "proposal_no_vote"
-	VoutProposalYesVote       VoutType = "proposal_yes_vote"
-	VoutPaymentRequestNoVote  VoutType = "payment_request_no_vote"
-	VoutPaymentRequestYesVote VoutType = "payment_request_yes_vote"
-	VoutPoolStaking           VoutType = "pool_staking"
+	VoutNonstandard               VoutType = "nonstandard"
+	VoutNulldata                  VoutType = "nulldata"
+	VoutPubkeyhash                VoutType = "pubkeyhash"
+	VoutPubkey                    VoutType = "pubkey"
+	VoutScripthash                VoutType = "scripthash"
+	VoutColdStaking               VoutType = "cold_staking"
+	VoutCfundContribution         VoutType = "cfund_contribution"
+	VoutProposalNoVote            VoutType = "proposal_no_vote"
+	VoutProposalYesVote           VoutType = "proposal_yes_vote"
+	VoutProposalAbstainVote       VoutType = "proposal_abstain_vote"
+	VoutPaymentRequestNoVote      VoutType = "payment_request_no_vote"
+	VoutPaymentRequestYesVote     VoutType = "payment_request_yes_vote"
+	VoutPaymentRequestAbstainVote VoutType = "payment_request_abstain_vote"
+	VoutPoolStaking               VoutType = "pool_staking"
 )
 
 var VoutTypes = map[string]VoutType{
-	"nonstandard":              VoutNonstandard,
-	"nulldata":                 VoutNulldata,
-	"pubkeyhash":               VoutPubkeyhash,
-	"pubkey":                   VoutPubkey,
-	"scripthash":               VoutScripthash,
-	"cold_staking":             VoutColdStaking,
-	"cfund_contribution":       VoutCfundContribution,
-	"proposal_no_vote":         VoutProposalNoVote,
-	"proposal_yes_vote":        VoutProposalYesVote,
-	"payment_request_no_vote":  VoutPaymentRequestNoVote,
-	"payment_request_yes_vote": VoutPaymentRequestYesVote,
-	"pool_staking":             VoutPoolStaking,
+	"nonstandard":                  VoutNonstandard,
+	"nulldata":                     VoutNulldata,
+	"pubkeyhash":                   VoutPubkeyhash,
+	"pubkey":                       VoutPubkey,
+	"scripthash":                   VoutScripthash,
+	"cold_staking":                 VoutColdStaking,
+	"cfund_contribution":           VoutCfundContribution,
+	"proposal_no_vote":             VoutProposalNoVote,
+	"proposal_yes_vote":            VoutProposalYesVote,
+	"proposal_abstain_vote":        VoutProposalAbstainVote,
+	"payment_request_no_vote":      VoutPaymentRequestNoVote,
+	"payment_request_yes_vote":     VoutPaymentRequestYesVote,
+	"payment_request_abstain_vote": VoutPaymentRequestAbstainVote,
+	"pool_staking":                 VoutPoolStaking,
 }
